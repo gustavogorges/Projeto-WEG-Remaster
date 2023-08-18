b0abda7400e1548dbec1003a01dd711840010f62
Gustavo Gorges Koch <gustavogk@weg.net>
2023-08-17 21:15:05 -0300

    tela projetos finalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'corNav' : '#E9EDF1'
+      }
+    },
   },
   plugins: [],
 }
